37f64aa747dd048ec312e02b94ccc1b940924d92
niceanpro <hianpro@gmail.com>
2022-02-20 09:11:45 +0900

    환경세팅완료

---
+++ src/test/java/com/anylogic/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anylogic.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
